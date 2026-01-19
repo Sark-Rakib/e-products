727eb3c92fbd6a088613a2d3f1a0bb2322891ae6
Sark-Rakib <mdrakibsarkar1@gmail.com>
2026-01-20 05:55:01 +0600

    set base url

--- frontend/src/lib/api.jsx
+++ frontend/src/lib/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const useAxios = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://e-products-gamma.vercel.app",
 });
 
 const useAxiosSecure = () => {
